195c5ed1da9a24d616e3f425fb23a7e01ed545c3
Jose Andres Leal <joseleal.fr0yo@gmail.com>
2024-02-18 15:43:29 -0500

    Offline Translator Project
    Created the base of a Offline Translator that uses ISO 639-1 language codes in order to translate the user inputted text and then provides the translation in the selected language. The user inputs are written onto a file and the outputs are also written onto a different file.
    
    The project was completed though the use of the translate library from pypi.org developed by Terry.Yin

---
+++ translatorProj/script.py
@@ -0,0 +1,22 @@
+from translate import Translator
+print('Enter the language you want to translate to: ')
+print('List of Available Languages: \n Afrikaans: af \n Catalan: ca \n Chinese: zh \n Dutch: nl \n French: fr \n German: de \n Greek: el \n Hebrew: he \n Hindi: hi \n Icelandic: is \n Italian: it \n Japanese: ja \n Kongo: kg \n Korean: ko \n Latin: la \n Norwegian: no \n Portuguese: pt \n Russian: ru \n Samoan: sm \n Spanish: es \n Swedish: sv \n')
+print('Enter the language code:')
+translator = Translator(to_lang=input(""))
+
+try:
+    # Create a file to store user input
+    with open('./txt4transl.txt', mode='w') as userInput:
+        inputText = input('Enter text to translate: ')
+        userInput.write(inputText)
+
+    # Reads the file and translates the text
+    with open('./txt4transl.txt', mode='r') as translateFile:
+        text = translateFile.read()
+        translated = translator.translate(text)
+    print(translated)
+
+    with open('./translation.txt', mode='w', encoding='utf-8') as translationFile:
+        translationFile.write(translated)
+except FileNotFoundError as err:
+    print('File not found')
