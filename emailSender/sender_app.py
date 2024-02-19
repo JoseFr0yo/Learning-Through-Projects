8d0273a2226478124ecd17625c208eca4bbc6a43
Jose Andres Leal <joseleal.fr0yo@gmail.com>
2024-02-19 12:05:42 -0500

    Email Sender Project
    Created the file for a Email Sender Project

---
+++ emailSender/sender_app.py
@@ -0,0 +1,4 @@
+# Import the EmailMessage class from the email.message module
+from email.message import EmailMessage
+
+sending_from = 'soyandyleal@gmail.com'
---
+++ passwordValidation/emailValidator.py
@@ -0,0 +1,10 @@
+import re
+
+pattern = re.compile(r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"
+                     )
+string = 'blob@blab.com'
+
+a = pattern.search(string)
+
+
+print(a)
